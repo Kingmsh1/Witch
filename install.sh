c1b863b95a98a2427133ad97b732093826aeaef3
root <shlkrtms1@gmail.com>
2026-06-12 15:32:27 -0400

    Add one-time installer file for Witch

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "[+] Installing WITCH repository..."
+
+REPO_URL="https://kingmsh1.github.io/Witch/"
+
+# Add repo list file
+echo "deb [trusted=yes] $REPO_URL ./" \
+| sudo tee /etc/apt/sources.list.d/witch.list > /dev/null
+
+echo "[+] Updating package lists..."
+sudo apt update
+
+echo "[+] Installing witch..."
+sudo apt install -y witch
+
+echo "[+] Installation complete!"
+echo "[+] Run: witch --help"
